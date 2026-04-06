db0c287747310309127e1a1775b16dfcc3715900
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-06 10:38:24 +1000

    fix: resolve component import  and arrange

--- VoltWise/src/pages/LandingPage.jsx
+++ VoltWise/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import WebFooter from '../components/WebFooter'
 import WebHeader from '../components/WebHeader'
+import LandingBody from '../components/LandingBody'
 
 function LandingPage() {
     return (
         <>
             <WebHeader />
+            <LandingBody />
             <WebFooter />
         </>
     )
