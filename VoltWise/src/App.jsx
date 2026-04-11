8de4dcf3e452da347101c71f2304c3e9f2758c59
P-Peeradon <p.determinant.integrate3@gmail.com>
2026-04-11 12:20:12 +1000

    chore: add route

--- VoltWise/src/App.jsx
+++ VoltWise/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router";
 import LandingPage from './pages/LandingPage';
 import UserRegisterPage from "./pages/UserRegisterPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/register" element={<UserRegisterPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
             </Routes>
         </BrowserRouter>
     )
